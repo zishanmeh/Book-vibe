749152f811edbf22c9988ef2dc93b3d3e62022e4
zishanmeh <zishanmehedihasan2@gmail.com>
2024-10-31 12:17:42 +0000

    Basic setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <span className="loading loading-spinner loading-xs"></span>
+      <span className="loading loading-spinner loading-sm"></span>
+      <span className="loading loading-spinner loading-md"></span>
+      <span className="loading loading-spinner loading-lg"></span>
+    </>
+  );
+}
+
+export default App;
