5c5fd5a75102f01e9ff773839fe03f71a432c167
zishanmeh <zishanmehedihasan2@gmail.com>
2024-10-31 12:37:45 +0000

    Added react router setup

---
+++ src/components/Erropage/ErrorPage.jsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+  return (
+    <div className="">
+      <h1 className="text-4xl text-center font-bold">Page not found</h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/Root/Root.jsx";
+import ErrorPage from "./components/Erropage/ErrorPage.jsx";
+import Home from "./components/Home/Home.jsx";
+import Dashboard from "./components/Dashboard/Dashboard.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard></Dashboard>,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
